98a4d3ce04d9b076515154ac1c9cf35c181d49eb
guti10501 <afgutierrez4@gmail.com>
2024-11-17 11:09:50 +0900

    5.7 Blog List Frontend, step 7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,48 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+  const [btnLabel, setBtnLabel] = useState('view')
+
+  const hideWhenVisible = {display: visible ? 'none' : ''}
+  const showWhenVisible = {display: visible ? '' : 'none'}
+
+  const showDetails = () => {
+    setVisible((preVisible) => {
+      const newVisible = !preVisible
+      setBtnLabel(newVisible ? 'hide' : 'view')
+      console.log("visible:",visible)
+      console.log("button label:",btnLabel)
+      return newVisible
+    })
+  }
+
+  const like = () => {
+    
+  }
+  
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title} {blog.author}
+        <button onClick={showDetails}>{btnLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}
+          <button onClick={like}>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
